fe3835aab3d9cd6219527961d1b9e68e689c0d56
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-07-28 20:55:07 +0900

    admin  작성중

--- src/test/java/org/maengle/model/ModelUpdateServiceTest.java
+++ src/test/java/org/maengle/model/ModelUpdateServiceTest.java
@@ -2,6 +2,7 @@ package org.maengle.model;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
+import org.maengle.admin.model.controllers.RequestModel;
 import org.maengle.global.libs.Utils;
 import org.maengle.model.entities.Model;
 import org.maengle.model.repositories.ModelRepository;
